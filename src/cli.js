09bbcc25e1aff539353f0a81fc14544cad61ab1f
Roman Rabinovich <roman@web-line.ru>
2022-09-10 08:38:06 +0300

    Refactor cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const greetings = () => {
+const greetings = () => {
     console.log('Welcome to the Brain Games!');
     
     const name = readlineSync.question('May I have your name? ');
     
     console.log(`Hello, ${name}!`);
 };
+
+export default greetings;
