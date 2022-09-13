359625fb14cd47dc84668e8a36e5aa44bb4a6048
Roman Rabinovich <roman@web-line.ru>
2022-09-13 22:27:37 +0300

    Add brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import Game from '../src/index.js';
+import { getRandomInt } from '../src/cli.js';
+
+class BrainPrimeGame extends Game {
+    constructor() {
+        super();
+        this.gameIntro = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    }
+
+    static #isPrime(n) {
+        for (let i = 2; i <= Math.sqrt(n); i += 1) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    generateQuestionAndAnswer() {
+        const number = getRandomInt(1, 1000);
+
+        this.questionPostfix = number;
+        this.correctAnswer = (BrainPrimeGame.#isPrime(number) % 2) === 0 ? 'yes' : 'no';
+    }
+}
+
+new BrainPrimeGame().start();
