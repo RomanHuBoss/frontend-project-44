e148fa88f06b269bdf484e5ce0a718fb58341319
Roman Rabinovich <roman@web-line.ru>
2022-09-14 23:16:24 +0300

    Change 4 spaces into 2 cause of linter's errors

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,30 +4,30 @@ import Game from '../src/index.js';
 import { getRandomInt } from '../src/cli.js';
 
 Game({
-    gameIntro: 'What number is missing in the progression?',
-    generator: () => {
-        const firstNumber = getRandomInt(1, 100);
-        const progressionLength = getRandomInt(5, 20);
-        const index2Hide = getRandomInt(0, progressionLength - 1);
-        const progressionStep = getRandomInt(1, 10);
+  gameIntro: 'What number is missing in the progression?',
+  generator: () => {
+    const firstNumber = getRandomInt(1, 100);
+    const progressionLength = getRandomInt(5, 20);
+    const index2Hide = getRandomInt(0, progressionLength - 1);
+    const progressionStep = getRandomInt(1, 10);
 
-        const result = {};
-        result.questionPostfix = '';
+    const result = {};
+    result.questionPostfix = '';
 
-        for (let i = 0; i < progressionLength; i += 1) {
-            if (i > 0) {
-                result.questionPostfix += ' ';
-            }
+    for (let i = 0; i < progressionLength; i += 1) {
+      if (i > 0) {
+        result.questionPostfix += ' ';
+      }
 
-            const elem = firstNumber + i * progressionStep;
+      const elem = firstNumber + i * progressionStep;
 
-            if (i === index2Hide) {
-                result.correctAnswer = String(elem);
-                result.questionPostfix += '..';
-            } else {
-                result.questionPostfix += elem;
-            }
-        }
-        return result;
-    },
+      if (i === index2Hide) {
+        result.correctAnswer = String(elem);
+        result.questionPostfix += '..';
+      } else {
+        result.questionPostfix += elem;
+      }
+    }
+    return result;
+  },
 });
